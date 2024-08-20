e4c16fdb815f4d4aaebec820bd7c906c05fdf67a
Oleh Kozhushko <oleg.k.linkedin@gmail.com>
2024-08-20 23:20:11 +0200

    Fixed method

--- tests/project/locators/ProductsPageLocators.ts
+++ tests/project/locators/ProductsPageLocators.ts
@@ -9,5 +9,24 @@ export class ProductsPageLocators {
         this.page = page;
         this.showResultsBtn = this.page.getByRole('link', { name: 'Показати' });
         this.productItemsList = this.page.locator('//div[@class="product-item__col-pic"]//a');
-    };
-}
+    }
+
+    getCategoryTab(category: string): Locator {
+        return this.page.getByRole('tab', { name: category });
+    }
+
+    getProductItem(productItem: string): Locator {
+        return this.page.getByRole('link', { name: productItem }).first();
+    }
+
+    categoryDropdown(filterCategory: string): Locator {  // Використання аргументу filterCategory
+        return this.page.locator(`//*[@data-bs-target and text()="${filterCategory}"]`);
+    }
+
+    filterItemCheckbox(filter: string): Locator {  // Використання аргументу filter
+        return this.page.locator(`(//label[text()="${filter}"])[1]/parent::div/label`);
+    }
+    filterCategoryLocator(filterCategory: string): Locator {
+        return this.page.locator(`//div[@class="filter-item__head" and contains(text(), "${filterCategory}")]`);
+    }
+};
--- tests/project/pages/ProductsPage.ts
+++ tests/project/pages/ProductsPage.ts
@@ -1,38 +1,36 @@
 import { Page } from "playwright";
 import { ProductsPageLocators } from '../locators/ProductsPageLocators';
 
-
 export class ProductsPage {
     private page: Page;
     private productsPageLocators: ProductsPageLocators;
 
-
     constructor(page: Page) {
         this.page = page;
         this.productsPageLocators = new ProductsPageLocators(page);
-
-    };
+    }
 
     async selectFilters(filterCategory: string, filter: string) {
-        const filterCategoryLocator = this.page.locator(
-            `//div[@class="filter-item__head" and contains(text(), "${filterCategory}")]`);
+        const filterCategoryLocator = this.productsPageLocators.filterCategoryLocator(filterCategory);
 
-        const isExpanded = await filterCategoryLocator.getAttribute(
-            "aria-expanded");
+        const isExpanded = await filterCategoryLocator.getAttribute("aria-expanded");
 
         if (isExpanded === "false") {
-            await this.page.locator(`//*[@data-bs-target and text() = ${filterCategory}]`).first().click();
+            await this.productsPageLocators.categoryDropdown(filterCategory)  // Використання методу з ProductsPageLocators
+                .first()
+                .click();
         }
-        await this.page
-            .locator(`(//label[text()="${filter}"])[1]/parent::div/label`)
+
+        await this.productsPageLocators.filterItemCheckbox(filter)  // Використання методу з ProductsPageLocators
             .click();
-    };
+    }
 
     async showResults() {
         await this.productsPageLocators.showResultsBtn.click();
-    };
+    }
+
     async selectItemByPosition(position: number) {
         const allItems = await this.productsPageLocators.productItemsList.all();
         allItems[position].click();
-    };
-};
+    }
+}
