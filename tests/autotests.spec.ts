0dcff1b78c6396943f033b95203ead4651bcbb04
Oleh Kozhushko <oleg.k.linkedin@gmail.com>
2024-08-21 14:54:41 +0200

    Added faker to tests

--- tests/autotests.spec.ts
+++ tests/autotests.spec.ts
@@ -3,12 +3,20 @@ import { MainPage } from "./project/pages/MainPage";
 import { ProductsPage } from "./project/pages/ProductsPage";
 import { ProductDetailsPage } from './project/pages/ProductDetailsPage';
 import { OrederPage } from './project/pages/OrderPage';
+import { faker } from '@faker-js/faker';
+
 test('Buy a product and check that request was created', async ({ page }) => {
 
   const mainPage = new MainPage(page);
   const productsPage = new ProductsPage(page);
   const productDetailsPage = new ProductDetailsPage(page);
   const orederPage = new OrederPage(page);
+
+  const randomFirtsName = faker.person.firstName;
+  const randomLastName = faker.person.lastName;
+  const randomMiddleName = faker.person.middleName;
+  const randomEmail = faker.internet.email;
+  const randomPhone = faker.phone.number;
 
   await mainPage.navigateToMainPage();
   await mainPage.selectLenguage();
@@ -21,10 +29,10 @@ test('Buy a product and check that request was created', async ({ page }) => {
   await productDetailsPage.placeOrder();
   await productDetailsPage.buyProduct();
   await orederPage.setContactInfo({
-    firstName: "John",
-    lastName: "Dee",
-    middleName: "Hilbertovich",
-    phone: "971010110",
-    email: "john.dee@gmail.com"
+    firstName: randomFirtsName(),
+    lastName: randomLastName(),
+    middleName: randomMiddleName(),
+    phone: randomPhone(),
+    email: randomEmail()
   });
 });
